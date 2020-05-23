3b0a43a862dfe1cbe2ce3f042df2e2e033825ca5
Luiz Augusto Melo <luiz@twistereventos.com>
2020-05-23 15:57:28 -0300

    Home page example in database, Add New Page, List of pages, Page Data Table Object, Page View Model are created and implemented

---
+++ Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,62 @@
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Declare list of PageVM
+            List<PageVM> pagesList;
+
+            //Init the Lits
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            //Return View with List
+            return View(pagesList);
+        }
+
+        // GET: Admin/Pages/AddPage
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+        // GET: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //check model state
+
+            //declare slug
+
+            //Init pageDTO
+
+            //DTO Title
+
+            //Check for and set slug if need be
+
+            //Make sure title and slug ar unique
+
+            //DTO Rest
+
+            //Save DTO
+
+            //Set TempData Message
+
+            //Redirect
+            return View();
+        }
+    }
+}
